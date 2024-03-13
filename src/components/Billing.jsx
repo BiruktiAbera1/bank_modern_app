c6edff8fbd2a75e4a6a16627af1f2afa37fb3424
Biruktawit Abera <birukspace0900@gmail.com>
2024-03-13 19:42:03 +0300

    Billing

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -5,36 +5,28 @@ const Billing = () => {
   return (
     <section id="product" className={layout.sectionReverse}>
       <div className={layout.sectionImgReverse}>
-        <img src={bill} alt="billing" className='w-[100%] h-[100%] relative z-[5]' />
+        <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
-        <div className='absolute z-[3] -left-1/2 top-0 w-[50%] rounded-full white__gradient'>
-        </div>
-        <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] rounded-full pink__gra'>
-        </div>
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] rounded-full white__gradient"></div>
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] rounded-full pink__gra"></div>
 
         <div className={layout.sectionInfo}>
           <h2 className={styles.heading2}>
-          Easily control your <br className="sm:block hidden" /> billing
-          & invoicing
+            Easily control your <br className="sm:block hidden" /> billing & invoicing
           </h2>
           <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-            Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
-            aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
-            placerat.
+            Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus
+            aliporttitor integer platea placerat.
           </p>
 
           <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-            <img src={apple} alt="google_play" className="w-[128px] 
-              h-[42px] object-contain mr-5 cursor-pointer" />
-            <img src={google} alt="google_play" className="w-[144px] 
-              h-[43px] object-contain cursor-pointer" />
+            <img src={apple} alt="google_play" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
+            <img src={google} alt="google_play" className="w-[144px] h-[43px] object-contain cursor-pointer" />
           </div>
         </div>
-       
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Billing
+export default Billing;
